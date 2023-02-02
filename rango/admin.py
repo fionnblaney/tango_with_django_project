32413c0833422a09b786078d9de7afd0d926467f
Fionn Blaney <2657865b@student.gla.ac.uk>
2023-02-02 17:39:46 +0000

    fourth commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+
+admin.site.register(Category)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
+    def __str__(self):
+        return self.name
+
+admin.site.register(Page, PageAdmin)
