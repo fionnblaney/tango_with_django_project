3547ec6af4cf5628bd8399261ca80a343e07f01a
Fionn Blaney <2657865b@student.gla.ac.uk>
2023-02-06 19:39:26 +0000

    fifth commit

--- rango/admin.py
+++ rango/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 
 
 class PageAdmin(admin.ModelAdmin):
--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -6,6 +7,11 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     class Meta:
         verbose_name_plural = 'Categories'
@@ -18,6 +24,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    
     
     def __str__(self):
         return self.title
